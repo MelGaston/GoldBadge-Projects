39cd4bea053774c344b8e73f89aae34443c611a0
Melanie Gaston <melaniegaston@Melanies-MacBook-Pro.local>
2018-12-18 16:42:30 -0500

    Rough draft done.

--- weekTwo/api.js
+++ weekTwo/api.js
@@ -5,15 +5,17 @@ var url;
 var spaceSearch = document.querySelector('.spaceSearch');
 var searchForm = document.querySelector('form');
 var submitBtn = document.querySelector('.submit');
-var newPictures = document.querySelector('.next');
-var resetAll = document.querySelector('.reset');
 var nav = document.querySelector('nav');
+var section = document.querySelector('section');
 var cardImg1 = document.querySelector('#pic1');
 var cardImg2 = document.querySelector('#pic2');
 var cardImg3 = document.querySelector('#pic3');
 var cardDesc1 = document.querySelector('#text1');
 var cardDesc2 = document.querySelector('#text2');
 var cardDesc3 = document.querySelector('#text3');
+
+nav.style.display = 'none';
+section.style.display = 'none';
 
 searchForm.addEventListener('submit', submitSearch);
 
@@ -41,11 +43,15 @@ function displayResults(json) {
     let pics = json.collection.items;
     var imageHolder = [];
     var descHolder = [];
+
+    var rando = Math.floor(Math.random() * (pics.length-3));
     
-    for(i=0;i<3;i++) {
-        imageHolder.push(pics[i].links[0].href);
-        descHolder.push(pics[i].data[0].title);
-    }
+    imageHolder.push(pics[rando].links[0].href);
+    imageHolder.push(pics[rando+1].links[0].href);
+    imageHolder.push(pics[rando+2].links[0].href);
+    descHolder.push(pics[rando].data[0].title);
+    descHolder.push(pics[rando+1].data[0].title);
+    descHolder.push(pics[rando+2].data[0].title);
 
     for(i=0;i<imageHolder.length;i+=3) {
         cardImg1.src = imageHolder[i];
@@ -56,9 +62,25 @@ function displayResults(json) {
         cardDesc3.innerHTML = descHolder[i+2];
     }
 
-    if(photoDestination.length > 0) {
+    if(cardImg1.src != "") {
+        section.style.display = 'flex';
         nav.style.display = 'block';
     } else {
         nav.style.display = 'none';
     }
+}
+
+function resetResults () {
+    cardImg1.src = "";
+    cardImg2.src = "";
+    cardImg3.src = "";
+    cardDesc1.innerHTML = "";
+    cardDesc2.innerHTML = "";
+    cardDesc3.innerHTML = "";
+    searchForm.reset();
+    imageHolder = [];
+    descHolder = [];
+
+    nav.style.display = 'none';
+    section.style.display = 'none';
 }
